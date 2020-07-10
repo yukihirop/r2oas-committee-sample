75d3e4cf338c6f5501d92cce88c1a7bcdb3e74ad
yukihirop <te108186@gmail.com>
2020-07-10 20:25:14 +0900

    Add description to tasks

---
+++ db/migrate/20200710112430_add_description_column_to_tasks.rb
@@ -0,0 +1,5 @@
+class AddDescriptionColumnToTasks < ActiveRecord::Migration[6.0]
+  def change
+    add_column :tasks, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_104059) do
+ActiveRecord::Schema.define(version: 2020_07_10_112430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_07_10_104059) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
